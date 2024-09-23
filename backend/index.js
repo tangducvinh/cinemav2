884f163f323c25ae6d976ca426a3b99c90b4d2c9
ducvinh <ducvinh100503@gmail.com>
2024-09-23 14:41:41 +0700

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.URL_CLIENT,
     methods: ["POST", "PUT", "GET", "DELETE"],
     credentials: true,
   })
