76682f293cc1932b32c2ecdd93755550894395da
ducvinh <ducvinh100503@gmail.com>
2024-09-29 16:05:04 +0700

    fix deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    // URL_SERVER_API: 'https://cinemav2-api.vercel.app/api',
-    URL_SERVER_API: "http://localhost:7000/api",
+    URL_SERVER_API: "https://cinemav2-api.vercel.app/api",
+    // URL_SERVER_API: "http://localhost:7000/api",
   },
 };
 
